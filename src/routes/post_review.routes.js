12377ee288fd802061aea163712f08ab22e865b4
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-08-21 15:27:41 +0530

    feat: enhance post review routes with approval and rejection functionalities, including authorization checks

--- src/routes/post_review.routes.js
+++ src/routes/post_review.routes.js
@@ -1,6 +1,35 @@
 import { Router } from "express";
+import {
+    approvePendingPost,
+    listAllPendingPost,
+    rejectPendingPost,
+} from "../controllers/post_rreview.controllers.js";
+import { authiddleware } from "../middlewares/auth.middleware.js";
+import { validatePermission } from "../middlewares/role.middleware.js";
+import { userRolesEnum } from "../utils/constant.js";
 
-const router = Router()
+const router = Router();
 
-export default router
+router
+    .route("/posts")
+    .post(
+        authiddleware,
+        validatePermission([userRolesEnum.ADMIN]),
+        listAllPendingPost,
+    );
+router
+    .route("/posts/:id/approve")
+    .put(
+        authiddleware,
+        validatePermission([userRolesEnum.ADMIN]),
+        approvePendingPost,
+    );
+router
+    .route("/posts/:id/reject")
+    .put(
+        authiddleware,
+        validatePermission([userRolesEnum.ADMIN]),
+        rejectPendingPost,
+    );
 
+export default router;
